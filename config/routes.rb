9671c3e028512b56c6864cd5ae6646536b2a0e53
Meighan Rasley <meighanr@gmail.com>
2015-12-31 11:44:03 -0800

    Add bcrypt, create users controller with new and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         get 'shopping_list', as: :list
       end
   end
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
