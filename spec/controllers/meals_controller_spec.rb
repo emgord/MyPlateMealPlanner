1c32dc51ebd678a7d579d2c9c4224b8d66bca2c7
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-01 20:57:52 -0800

    added first spec and simplecov config to spec_helper

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe MealsController, type: :controller do
+  describe "GET #index" do
+    it "renders the index view" do
+      get :index
+      expect(subject).to render_template :index
+    end
+  end
+
 
 end
