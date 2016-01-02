d21eea6f5a02875f89df6841de23b669a8b0c3b3
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-01 21:30:04 -0800

    added spec for destroy and fixed a bug

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -21,46 +21,60 @@ RSpec.describe MealsController, type: :controller do
   end
 
   describe "POST 'create'" do
-      let(:params) do
-        {
-          meal: {
-            meal: "Dinner",
-            day: "Friday",
-            meal_name: "Mac and Cheese",
-            description: "Mmm...cheese"
-          }
+    let(:params) do
+      {
+        meal: {
+          meal: "Dinner",
+          day: "Friday",
+          meal_name: "Mac and Cheese",
+          description: "Mmm...cheese"
         }
-      end
-
-      it "redirects to the show view" do
-        post :create, params
-        expect(subject).to redirect_to meal_path(1)
-      end
+      }
     end
 
-    describe "GET 'show'" do
-      it "renders the show view" do
-        get :show, id: meal.id
-        expect(subject).to render_template :show
-      end
+    it "redirects to the show view" do
+      post :create, params
+      expect(subject).to redirect_to meal_path(1)
+    end
+  end
+
+  describe "GET 'show'" do
+    it "renders the show view" do
+      get :show, id: meal.id
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "GET #edit" do
+    it "renders the edit view" do
+      get :edit, id: meal.id
+      expect(subject).to render_template :edit
+    end
+  end
+
+  describe "PATCH 'update'" do
+    let(:updated_params) do
+      {
+        meal: {
+          meal: "Dinner",
+          day: "Friday",
+          meal_name: "Mac and Cheese",
+          description: "Now even cheesier!"
+        }
+      }
     end
 
-    describe "PATCH 'update'" do
-        let(:updated_params) do
-          {
-            meal: {
-              meal: "Dinner",
-              day: "Friday",
-              meal_name: "Mac and Cheese",
-              description: "Now even cheesier!"
-            }
-          }
-        end
+    it "redirects to the show view" do
+      patch :update, {id: meal.id, meal: updated_params}
+      expect(subject).to redirect_to meal_path(1)
+    end
+  end
 
-        it "redirects to the show view" do
-          post :create, updated_params
-          expect(subject).to redirect_to meal_path(1)
-        end
-      end
+  describe "DELETE 'destroy'" do
+    it "redirects to the root path" do
+      delete :destroy, id: meal.id
+      expect(subject).to redirect_to root_path
+    end
+  end
 
 end
