9ffd8c525e24ad52aed5ec437f0d8b8bb9817c54
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-01 21:08:36 -0800

    added specs for new and create

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -8,5 +8,30 @@ RSpec.describe MealsController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "renders the new view" do
+      get :new
+      expect(subject).to render_template :new
+    end
+  end
+
+
+  describe "POST 'create'" do
+      let(:params) do
+        {
+          meal: {
+            name: "Dinner",
+            day: "Friday",
+            meal_name: "Mac and Cheese",
+            description: "Mmm...cheese"
+          }
+        }
+      end
+
+      it "redirects to the show view" do
+        post :create, params
+        expect(subject).to redirect_to meal_path(1)
+      end
+    end
 
 end
