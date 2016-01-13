824c6991973018fd61c3459aa534d8190687273c
emgord <emgord@gmail.com>
2016-01-12 21:04:25 -0800

    added spec for meal model

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -2,7 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
 
-  # meal = build(:meal) # matches the factory name, similar to new, will not save
-  # meal = create(:meal) # creates and saves to db
+  let(:ingredient) { create(:ingredient) }
+  let(:meal) { ingredient.meal }
+
+  describe "#food_groups" do
+    it "returns an array with the food groups for the meal" do
+      expect(meal.food_groups).to eq(["Dairy"])
+    end
+  end
+
+  describe "#needed_food_groups" do
+    it "returns an array with the food groups missing from the meal" do
+      expect(meal.needed_food_groups).to eq(["Grains", "Protein", "Vegetables", "Fruits"])
+    end
+  end
+
+  describe "#needed" do
+    it "returns selected if the meal has a food group" do
+      expect(meal.needed("Dairy")).to eq("selected")
+    end
+    it "returns needed if the meal does not have a food group" do
+      expect(meal.needed("Fruits")).to eq("needed")
+    end
+  end
+
+  describe "#food_group_count" do
+    it "returns the number of food groups a meal has" do
+      expect(meal.food_group_count).to eq(1)
+    end
+  end
 
 end
